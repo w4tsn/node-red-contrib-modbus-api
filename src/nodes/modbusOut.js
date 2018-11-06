981902fb5a58b3d3285f50bdcfe5771d8f9c9b0a
Alexander Wellbrock <lionax@mailbox.org>
2018-11-06 18:03:38 +0100

    Fix modbus out error when trying to respond with 0

--- src/nodes/modbusOut.js
+++ src/nodes/modbusOut.js
@@ -5,9 +5,9 @@ module.exports = function (RED) {
         let node = this;
         node.on('input', msg => {
             let errors = 0;
-            if (!msg.res) { node.error('Malformed input! Input msg has no property res!'); errors++; }
-            if (msg.res && !msg.res.callback) { node.error('Malformed input! Input msg has no property res.callback!'); errors++; }
-            if (!msg.payload) { node.error('Malformed input! Input msg has no payload to provide!'); errors++; }
+            if (msg.res === null) { node.error('Malformed input! Input msg has no property res!'); errors++; }
+            if (msg.res !== null && msg.res.callback === null) { node.error('Malformed input! Input msg has no property res.callback!'); errors++; }
+            if (msg.payload === null) { node.error('Malformed input! Input msg has no payload to provide!'); errors++; }
             if (errors > 0) { return msg.res.callback('ERROR: No value provided.'); }
             else { return msg.res.callback(null, msg.payload); }
         });
